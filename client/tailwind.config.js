390b249d9a359dda5925b95583ae7c467eac35c3
Richer <webgame72@gmail.com>
2024-11-03 18:37:05 +0500

    fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */;
+
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#0F172A",
-        secondary: "#64748B",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{html,js,jsx}"],
+	theme: {
+		extend: {
+			colors: {
+				current: "#000000e6",
+				primary: {
+					DEFAULT: "#5597f2",
+					45: "rgba(0, 85, 255, 0.45)",
+					50: "rgba(0, 85, 255, 0.5)",
+				},
+				brown: {
+					DEFAULT: "#443e38",
+				},
+				white: {
+					DEFAULT: "#FFFFFF",
+					50: "rgba(255, 255, 255, 0.5)",
+					65: "rgba(255, 255, 255, 0.65)",
+					85: "rgba(255, 255, 255, 0.85)",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
